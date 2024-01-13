9004cd440f7caef9ccfa08e11588e6f055794065
Nicolas <nicolas.pp333@gmail.com>
2024-01-13 11:55:16 -0300

    Class 54

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -13,9 +13,45 @@ namespace Portafolio.Controllers
             _logger = logger;
         }
 
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                new Proyecto
+            {
+                Titulo = "Amazon",
+                Descripcion = "E-Commerce realizado en ASP.NET Core",
+                Link = "https://amazon.com",
+                ImagenURL = "/imagenes/amazon.png"
+            },
+                new Proyecto
+            {
+                Titulo = "New York Times",
+                Descripcion = "Pagina de noticias en React",
+                Link = "https://nytimes.com",
+                ImagenURL = "/imagenes/nyt.png"
+            },
+                new Proyecto
+            {
+                Titulo = "Reddit",
+                Descripcion = "Red social para compartir en comunidades",
+                Link = "https://reddit.com",
+                ImagenURL = "/imagenes/reddit.png"
+            },
+                new Proyecto
+            {
+                Titulo = "Steam",
+                Descripcion = "Tienda en lina para comprar videojuegos",
+                Link = "https://store.steampowered.com",
+                ImagenURL = "/imagenes/steam.png"
+            }
+            };
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
         }
 
         public IActionResult Privacy()
--- Portafolio/Models/ErrorViewModel.cs
+++ Portafolio/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Portafolio.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
