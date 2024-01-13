d691217c06d7098a8927064a20accf778e011f95
Nicolas <nicolas.pp333@gmail.com>
2024-01-13 18:49:07 -0300

    Classs 60

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
@@ -7,49 +8,17 @@ namespace Portafolio.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos _repositorioProyectos;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
         {
             _logger = logger;
-        }
-
-        private List<Proyecto> ObtenerProyectos()
-        {
-            return new List<Proyecto>() {
-                new Proyecto
-            {
-                Titulo = "Amazon",
-                Descripcion = "E-Commerce realizado en ASP.NET Core",
-                Link = "https://amazon.com",
-                ImagenURL = "/imagenes/amazon.png"
-            },
-                new Proyecto
-            {
-                Titulo = "New York Times",
-                Descripcion = "Pagina de noticias en React",
-                Link = "https://nytimes.com",
-                ImagenURL = "/imagenes/nyt.png"
-            },
-                new Proyecto
-            {
-                Titulo = "Reddit",
-                Descripcion = "Red social para compartir en comunidades",
-                Link = "https://reddit.com",
-                ImagenURL = "/imagenes/reddit.png"
-            },
-                new Proyecto
-            {
-                Titulo = "Steam",
-                Descripcion = "Tienda en lina para comprar videojuegos",
-                Link = "https://store.steampowered.com",
-                ImagenURL = "/imagenes/steam.png"
-            }
-            };
+            _repositorioProyectos = repositorioProyectos;
         }
 
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var proyectos = _repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
